30f213f841ddecd434549a754fb5b4cc47c5c3f0
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-20 23:42:44 -0300

    Initialize repository. Add API project, gitignore and solution file.

---
+++ ContosoPets.Api/Startup.cs
@@ -0,0 +1,66 @@
+using ContosoPets.DataAccess.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using ContosoPets.DataAccess.Services;
+
+namespace ContosoPets.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add the OrderService DI registration code
+            services.AddScoped<OrderService>();
+
+            var builder = new SqlConnectionStringBuilder(
+                Configuration.GetConnectionString("ContosoPets"));
+            IConfigurationSection contosoPetsCredentials =
+                Configuration.GetSection("ContosoPetsCredentials");
+
+            builder.UserID = contosoPetsCredentials["UserId"];
+            builder.Password = contosoPetsCredentials["Password"];
+
+            services.AddDbContext<ContosoPetsContext>(options =>
+                options.UseSqlServer(builder.ConnectionString)
+                       .EnableSensitiveDataLogging(Configuration.GetValue<bool>("Logging:EnableSqlParameterLogging")));
+
+            services.AddControllers();
+            services.AddApplicationInsightsTelemetry();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
